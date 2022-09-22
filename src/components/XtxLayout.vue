14af52ef3ad7194a9f7659252554b28bef7df207
lidoudoou <lidoudoou@gmail.com>
2022-09-22 16:02:24 +0800

    start

---
+++ src/components/XtxLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import XtxTopNav from "@/components/XtxTopNav.vue";
+import XtxHeader from "@/components/XtxHeader.vue";
+import XtxFooter from "@/components/XtxFooter.vue";
+</script>
+<template>
+  <XtxTopNav />
+  <XtxHeader />
+  <RouterView />
+  <XtxFooter />
+</template>
